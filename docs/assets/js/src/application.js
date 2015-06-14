e11868a1e13d97e6447bddcbcb785f3b503483ac
Felix Fennell <felix@felixfennell.co.uk>
2015-06-14 22:44:11 +0100

    Cleaning up and rewriting documentation
    This is a first pass over the end-user documentation which will be a published Jekyll site.
    
    The original bootstrap docs will be used as a base, as they provide details on all the components etc. that can be used. The idea is for this documentation to be a one-stop-shop, so rather than redirect people to the components included in Bootstrap by default, and only highlight our custom components, we will show everything in one place, regardless of where it came from.
    
    This means we will simply adapt much of Boostrap's documentation and add our additions as needed. There are some places where more work is required however, the homepage and getting-started page. These have both been significantly modified and i'm more or less happy with them.
    
    The bulk of the work will now be updating the CSS and components pages as needed to integrate Helpful's styles and in clarifying/enhancing the bootstrap documentation wherever this may be lacking.
    
    Although currently this looks like we're just stealing Bootstrap and find/replacing references to it, this is a first pass to tidy up anything we won't be doing (customising downloads for example). As time goes on our own, bespoke, content and customisations will increase and provide actual value over just reading the bootstrap docs themselves.
    
    This does mean we need to careful when bootstrap updates such that we don't have to do all this again to incorporate updates - though this isn't a concern for the short term.
    
    Further documentations will be made in the next few weeks to review everything, at least to a superficial level.

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -56,33 +56,6 @@
       $('.bs-top').affix()
     }, 100)
 
-    // theme toggler
-    ;(function () {
-      var $stylesheetLink = $('#bs-theme-stylesheet')
-      var $themeBtn = $('.bs-docs-theme-toggle')
-
-      var activateTheme = function () {
-        $stylesheetLink.attr('href', $stylesheetLink.attr('data-href'))
-        $themeBtn.text('Disable theme preview')
-        localStorage.setItem('previewTheme', true)
-      }
-
-      if (localStorage.getItem('previewTheme')) {
-        activateTheme()
-      }
-
-      $themeBtn.click(function () {
-        var href = $stylesheetLink.attr('href')
-        if (!href || href.indexOf('data') === 0) {
-          activateTheme()
-        } else {
-          $stylesheetLink.attr('href', '')
-          $themeBtn.text('Preview theme')
-          localStorage.removeItem('previewTheme')
-        }
-      })
-    })();
-
     // Tooltip and popover demos
     $('.tooltip-demo').tooltip({
       selector: '[data-toggle="tooltip"]',
@@ -123,52 +96,6 @@
     // Activate animated progress bar
     $('.bs-docs-activate-animated-progressbar').on('click', function () {
       $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('active')
-    })
-
-    // Config ZeroClipboard
-    ZeroClipboard.config({
-      moviePath: '/assets/flash/ZeroClipboard.swf',
-      hoverClass: 'btn-clipboard-hover'
-    })
-
-    // Insert copy to clipboard button before .highlight
-    $('.highlight').each(function () {
-      var btnHtml = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>'
-      $(this).before(btnHtml)
-    })
-    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'))
-    var $htmlBridge = $('#global-zeroclipboard-html-bridge')
-
-    // Handlers for ZeroClipboard
-    zeroClipboard.on('load', function () {
-      $htmlBridge
-        .data('placement', 'top')
-        .attr('title', 'Copy to clipboard')
-        .tooltip()
-
-
-      // Copy to clipboard
-      zeroClipboard.on('dataRequested', function (client) {
-        var highlight = $(this).parent().nextAll('.highlight').first()
-        client.setText(highlight.text())
-      })
-
-      // Notify copy success and reset tooltip title
-      zeroClipboard.on('complete', function () {
-        $htmlBridge
-          .attr('title', 'Copied!')
-          .tooltip('fixTitle')
-          .tooltip('show')
-          .attr('title', 'Copy to clipboard')
-          .tooltip('fixTitle')
-      })
-    })
-
-    // Hide copy button when no Flash is found
-    // or wrong Flash version is present
-    zeroClipboard.on('noflash wrongflash', function () {
-      $('.zero-clipboard').remove()
-      ZeroClipboard.destroy()
     })
 
   })
