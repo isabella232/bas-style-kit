1384dc23560cdec1ab7e08845dc4fa5bec9b60f3
Felix Fennell <felnne@bas.ac.uk>
2015-11-04 09:59:20 +0000

    Adding new IAM users and policies for deployments
    This removes the need to use a single IAM user with permissions to deploy to both Production and Staging environments.
    
    This is in preparation to moving to using Semaphore's S3 deployment strategy, at which point the legacy IAM user and policy can be removed. This means Ansible Vault is then no longer needed for this project.
    
    Our Terraform configuration is also tidied up.

--- terraform.tf
+++ terraform.tf
@@ -7,7 +7,6 @@ variable "aws_secret_key" {}  # Define using environment variable - e.g. TF_VAR_
 
 # Providers
 
-# AWS provider configuration
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
@@ -17,10 +16,17 @@ provider "aws" {
 
 # Resources
 
-# `bas-style-kit-docs-stage` resource
+# Static website for staging documentation (Global)
 
-# S3 bucket with static website hosting
-resource "aws_s3_bucket" "bas-style-kit-docs-stage-bucket" {
+resource "aws_iam_user" "semmaphore-deploy-bas-style-kit-docs-stage" {
+    name = "semmaphore-deploy-bas-style-kit-docs-stage"
+}
+resource "aws_iam_user_policy" "semmaphore-deploy-bas-style-kit-docs-stage" {
+    name = "semmaphore-deploy-bas-style-kit-docs-stage"
+    user = "${aws_iam_user.semmaphore-deploy-bas-style-kit-docs-stage.name}"
+    policy = "${file("provisioning/data/iam-inline-policies/semmaphore-deploy-bas-style-kit-docs-stage.json")}"
+}
+resource "aws_s3_bucket" "bas-style-kit-docs-stage" {
   bucket = "bas-style-kit-docs-stage"
   acl = "public-read"
   policy = "${file("provisioning/data/S3-bucket-policies/bas-style-kit-docs-stage.json")}"
@@ -30,10 +36,17 @@ resource "aws_s3_bucket" "bas-style-kit-docs-stage-bucket" {
   }
 }
 
-# `bas-style-kit-docs-prod` resource
+# Static website for production documentation (Global)
 
-# S3 bucket with static website hosting
-resource "aws_s3_bucket" "bas-style-kit-docs-prod-bucket" {
+resource "aws_iam_user" "semmaphore-deploy-bas-style-kit-docs-prod" {
+    name = "semmaphore-deploy-bas-style-kit-docs-prod"
+}
+resource "aws_iam_user_policy" "semmaphore-deploy-bas-style-kit-docs-prod" {
+    name = "semmaphore-deploy-bas-style-kit-docs-prod"
+    user = "${aws_iam_user.semmaphore-deploy-bas-style-kit-docs-prod.name}"
+    policy = "${file("provisioning/data/iam-inline-policies/semmaphore-deploy-bas-style-kit-docs-prod.json")}"
+}
+resource "aws_s3_bucket" "bas-style-kit-docs-prod" {
   bucket = "bas-style-kit-docs-prod"
   acl = "public-read"
   policy = "${file("provisioning/data/S3-bucket-policies/bas-style-kit-docs-prod.json")}"
@@ -41,4 +54,15 @@ resource "aws_s3_bucket" "bas-style-kit-docs-prod-bucket" {
     index_document = "index.html"
     error_document = "error.html"
   }
+}
+
+# BAS CDN user for distributing assets (Global)
+
+resource "aws_iam_user" "semmaphore-deploy-bas-cdn-prod" {
+    name = "semmaphore-deploy-bas-cdn-prod"
+}
+resource "aws_iam_user_policy" "semmaphore-deploy-bas-cdn-prod" {
+    name = "semmaphore-deploy-bas-cdn-prod"
+    user = "${aws_iam_user.semmaphore-deploy-bas-cdn-prod.name}"
+    policy = "${file("provisioning/data/iam-inline-policies/semmaphore-deploy-cdn-prod.json")}"
 }
