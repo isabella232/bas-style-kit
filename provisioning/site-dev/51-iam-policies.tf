6d7dcfc7f9f0d06ecb16dbeedc41370c67ed6c75
Felix Fennell <felix@felixfennell.co.uk>
2017-02-19 20:20:27 +0000

    Fixing permissions with new Package Service buckets

--- provisioning/site-dev/51-iam-policies.tf
+++ provisioning/site-dev/51-iam-policies.tf
@@ -15,9 +15,9 @@
 # AWS source: https://aws.amazon.com/iam/
 # Terraform source: https://www.terraform.io/docs/providers/aws/r/iam_user_policy.html
 resource "aws_iam_user_policy" "upload-snapshot-bas-style-kit-dev" {
-    name = "upload-snapshot-dev"
-    user = "bas-gitlab-deploy-bas-style-kit"
-    policy = "${file("51-iam-policy-upload-bas-packages-dev.json")}"
+  name   = "upload-snapshot-dev"
+  user   = "bas-gitlab-deploy-bas-style-kit"
+  policy = "${file("51-iam-policy-upload-bas-package-service-prod.json")}"
 }
 
 # In-line user policy
@@ -31,8 +31,7 @@ resource "aws_iam_user_policy" "upload-snapshot-bas-style-kit-dev" {
 # AWS source: https://aws.amazon.com/iam/
 # Terraform source: https://www.terraform.io/docs/providers/aws/r/iam_user_policy.html
 resource "aws_iam_user_policy" "upload-assets-bas-style-kit-dev" {
-    name = "upload-assets-dev"
-    user = "bas-gitlab-deploy-bas-style-kit"
-    policy = "${file("51-iam-policy-upload-bas-cdn-dev.json")}"
+  name   = "upload-assets-dev"
+  user   = "bas-gitlab-deploy-bas-style-kit"
+  policy = "${file("51-iam-policy-upload-bas-cdn-dev.json")}"
 }
-
